861f2d3339fb1e6f493fbbecfccc13dbb63e2ef3
archana <thaarchana7jan@gmail.com>
2023-08-20 23:37:14 +0530

    cors issue

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -12,13 +12,9 @@ const cors = Cors({
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
      // Using the cors middleware
      await runMiddleware(req, res, cors);
-     res.setHeader('Access-Control-Allow-Credentials', 'true')
-     res.setHeader('Access-Control-Allow-Origin', '*')
-     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-     res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
     if (req.method === 'GET') {
         const getPostsQuery = `SELECT * FROM posts`;
