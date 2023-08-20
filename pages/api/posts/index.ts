583c0e9d893370aeae12bb2edfa91bc8bdb0ee92
archana <thaarchana7jan@gmail.com>
2023-08-20 23:22:09 +0530

    config updated

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -8,6 +8,13 @@ import { getPostByID, runMiddleware, cors } from '../../../utils/commonFunction'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
      // Using the cors middleware
      await runMiddleware(req, res, cors);
+     res.setHeader('Access-Control-Allow-Credentials', 'true')
+     res.setHeader('Access-Control-Allow-Origin', '*')
+     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+     res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     
     if (req.method === 'GET') {
         const getPostsQuery = `SELECT * FROM posts`;
